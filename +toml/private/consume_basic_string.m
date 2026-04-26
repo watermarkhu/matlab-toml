f33f9bd3cd2b91b5a13cf2cf5d62d1a463decfd5
watermarkhu <watermarkhu@gmail.com>
2026-04-26 13:21:22 +0200

    feat: TOML 1.1 string escapes \e and \xHH
    - valid/string/escape-esc: \e => U+001B
    - valid/string/hex-escape: \xHH for codepoints 0x00-0xFF

--- +toml/private/consume_basic_string.m
+++ +toml/private/consume_basic_string.m
@@ -49,6 +49,16 @@ function [content, str] = terminate_string(str, is_multiline)
             pieces{end+1} = c;
           case { 'b', 't', 'r', 'f', 'n' }
             pieces{end+1} = sprintf(['\' c]);
+          case 'e'
+            pieces{end+1} = char(0x1B);
+          case 'x'
+            [code_point, str] = get_hex_digits(str, 2);
+            code_point = uint32(hex2dec(code_point));
+            if is_octave()
+              pieces{end+1} = utf8ify(code_point);
+            else
+              pieces{end+1} = char(code_point);
+            end
           case { 'u', 'U' }
             num_digits = 4;
             if c == 'U'
